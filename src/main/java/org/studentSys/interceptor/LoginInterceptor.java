f1ab1566109e7c639a50c0b8b69a2e4fec5533b5
huija <1150555483@qq.com>
2018-10-27 14:54:44 +0800

    comment about deploy in interceptor

--- src/main/java/org/studentSys/interceptor/LoginInterceptor.java
+++ src/main/java/org/studentSys/interceptor/LoginInterceptor.java
@@ -17,8 +17,8 @@ public class LoginInterceptor implements HandlerInterceptor {
         if (session.getAttribute("student") == null
                 && session.getAttribute("teacher") == null
                 && session.getAttribute("parent") == null) {
-            httpServletResponse.sendRedirect("/student_sys/login");
-            //这个是和重定向相同,/login前面设置的是你部署所在的url(去掉ip和端口),具体看你tomcat部署的设置,我设置的是student_sys
+            httpServletResponse.sendRedirect("/login");
+            // //前面可以加你自己部署的项目url位置,具体的看自己tomcat的设置,默认直接/
         }
         return true;
     }
