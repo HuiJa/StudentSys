e275e64551dde4dd7903afb6e790cb113ecd7cdd
huija <1150555483@qq.com>
2019-03-17 18:53:51 +0800

    eureka_test

--- eureka_client/src/main/java/org/huija/eureka_client/EurekaClientApplication.java
+++ eureka_client/src/main/java/org/huija/eureka_client/EurekaClientApplication.java
@@ -1,4 +1,4 @@
-package org.studentSys;
+package org.huija.eureka_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class StudentSysApplication {
+public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudentSysApplication.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 
 }
