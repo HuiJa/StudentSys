0c33984c087a87bb57218372a1202cea799448cd
huija <1150555483@qq.com>
2019-03-21 22:44:33 +0800

    slim_the_zip

--- src/main/java/org/studentSys/web/DataApi.java
+++ src/main/java/org/studentSys/web/DataApi.java
@@ -3,6 +3,7 @@ package org.studentSys.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.studentSys.service.StudentService;
 /**
  * Created by HuiJa on 2018/5/27.
  */
+//@CrossOrigin
 @RestController
 @RequestMapping(value = "/api")
 public class DataApi {
